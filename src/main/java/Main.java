767e20cd4931426dac71f7da0d54a30d1e29c16f
Gilbert Baracka <gilbert.baracka@hsc.utah.edu>
2024-05-31 11:02:58 -0600

    cleaninput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Utility.userInteraction();
+        Utility utility = new Utility();
+        utility.userInteraction();
     }
 }
--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -4,9 +4,24 @@ public class Utility {
     private static final Scanner scanner = new Scanner(System.in);
     private Task task = new Task();
 
-    public static void userInteraction(){
+    public void userInteraction() {
         System.out.println("Type title of task");
-        String title = scanner.nextLine();
-        System.out.println(title);
+        String title = this.cleanInput();
+        System.out.println(title instanceof String);
+    }
+
+    //validates if the input value is longer than five characters
+    private String cleanInput(){
+        while(true){
+            try{
+                String userInput = scanner.nextLine();
+                if(userInput.length() < 5){
+                    throw new Exception();
+                }
+                return userInput;
+            } catch (Exception e){
+                System.out.println("length must be greater than 5");
+            }
+        }
     }
 }
