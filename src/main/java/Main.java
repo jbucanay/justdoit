64e0f0913db500695130c320a02594ae003422fc
Gilbert Baracka <gilbert.baracka@gmail.com>
2024-06-03 09:17:22 -0600

    added git ignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
+
 
 public class Main {
     public static void main(String[] args) {
